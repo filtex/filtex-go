f9d23860f2bac75e7ae135364bdd76e322200a43
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 13:55:51 +0300

    feat: create postgres end with and not end with operator impls

---
+++ builders/postgres/operators/not_end_with_operator.go
@@ -0,0 +1,21 @@
+package operators
+
+import (
+	"fmt"
+
+	"github.com/filtex/filtex-go/builders/postgres/types"
+	"github.com/filtex/filtex-go/constants"
+)
+
+type NotEndWithOperator struct{}
+
+func (NotEndWithOperator) Build(fieldType constants.FieldType, field string, value interface{}, index int) *types.PostgresExpression {
+	if fieldType != constants.FieldTypeString {
+		return nil
+	}
+
+	return &types.PostgresExpression{
+		Condition: fmt.Sprintf("%s NOT ILIKE '%%' || $%v", field, index),
+		Args:      []interface{}{value},
+	}
+}
