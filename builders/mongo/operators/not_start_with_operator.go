555e71d1492a90303218bedc281e255c39402938
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-07 12:25:12 +0300

    feat: create mongo start with and not start with operator impls

---
+++ builders/mongo/operators/not_start_with_operator.go
@@ -0,0 +1,29 @@
+package operators
+
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/filtex/filtex-go/builders/mongo/types"
+	"github.com/filtex/filtex-go/constants"
+)
+
+type NotStartWithOperator struct{}
+
+func (NotStartWithOperator) Build(fieldType constants.FieldType, field string, value interface{}) *types.MongoExpression {
+	if fieldType != constants.FieldTypeString {
+		return nil
+	}
+
+	return &types.MongoExpression{
+		Condition: bson.M{
+			field: bson.M{
+				"$not": bson.M{
+					"$regex":   fmt.Sprintf("^%s", value),
+					"$options": "i",
+				},
+			},
+		},
+	}
+}
